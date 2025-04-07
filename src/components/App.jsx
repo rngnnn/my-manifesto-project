750a9fac60adfa081726ed07aa4640e6093c2a50
rngnnn <rngnnnclk@gmail.com>
2025-04-07 20:21:02 +0200

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      {/* Diğer bileşenler */}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
